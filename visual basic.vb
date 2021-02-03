ca7f05d1246a5a729918c91d2207433e1e808d51
AbdullahIqbal103289 <75801854+AbdullahIqbal103289@users.noreply.github.com>
2021-02-03 10:26:51 -0800

    Add files via upload
    MEMBERS OF THE FAMILY USE SAME LAPTOP . EACH FAMILY MEMBER HAS THEIR OWN PASSWORD . 
    TO BE VALID PASSWORD MUST COMPLY THE FOLLOWING RULES:-
    1] AT LEAST 2 LOWER CASE ALPHABETIC CHARACTERS
    2]ATLEAST 2 UPPER CASE ALPHABETIC CHARACTERS
    3]AT LEAST THREE NUMERIC ADDRESS
    RETURN THE BOOLEAN VALUE 
    TRUE if it is a valid password
    FALSE if it is a invalid password

---
+++ visual basic.vb
@@ -0,0 +1,50 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mychar1, mychar2 As Char
+        Dim mystr As String
+        Dim count, A, B As Integer
+        Dim i As Integer
+        Dim isvalid As Boolean
+        mystr = ""
+        count = 0
+        mychar2 = ""
+        B = 0
+        A = 0
+        mychar1 = ""
+        i = 0
+        isvalid = True
+        Console.Write("PLEASE ENTER PASSWORD : ")
+        mystr = Console.ReadLine
+
+        For i = 1 To Len(mystr)
+            mychar1 = Mid(mystr, i, 1)
+            If Asc(mychar1) >= 97 And Asc(mychar1) <= 122 Then
+                count = count + 1
+            End If
+        Next
+        For i = 1 To Len(mystr)
+            mychar1 = Mid(mystr, i, 1)
+            If Asc(mychar1) >= 65 And Asc(mychar1) <= 90 Then
+                A = A + 1
+            End If
+        Next
+        For i = 1 To Len(mystr)
+            mychar2 = Mid(mystr, i, 1)
+            If Asc(mychar2) >= 48 And Asc(mychar2) <= 57 Then
+                B = B + 1
+            End If
+        Next
+        If count = 2 And A = 2 And B = 3 Then
+            isvalid = True
+        Else
+            isvalid = False
+
+        End If
+
+        Console.WriteLine("ENTERED PASSWORD IS: " & isvalid)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
